fbf4b0af402e51981030055b6957a2f0462017b6
UtsavEshan <53523427+UtsavEshan@users.noreply.github.com>
2020-04-01 05:52:41 +0530

    finished

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'location_screen.dart';
+import 'package:clima/services/weather.dart';
+
+
+
+
+class LoadingScreen extends StatefulWidget {
+
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocationData();
+  }
+
+   void getLocationData() async {
+
+    var weatherData = await WeatherModel().getLocationWeather();
+
+    Navigator.push(context, MaterialPageRoute(builder:(context){
+      return LocationScreen(
+         locationWeather: weatherData,
+      );
+    }));
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
+  }
+}
+
+//var weather = jsonDecode(data)['main']['temp'];
+//var condition = jsonDecode(data)['weather'][0]['id'];
+//var city =  jsonDecode(data)['name'];
+//print(weather);
+//print(condition);
+//print(city);
